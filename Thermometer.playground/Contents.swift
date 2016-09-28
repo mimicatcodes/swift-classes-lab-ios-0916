04c4afac59a3e3b7e09ba2873fcafda88ee0f82c
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-09-28 10:10:46 -0400

    Done

--- Thermometer.playground/Contents.swift
+++ Thermometer.playground/Contents.swift
@@ -13,6 +13,30 @@
  To start, define a class called `Thermometer`. This class should contain one property, `fahrenheit` (a `Double`), to hold the temperature represented by the thermometer. This property should be a _variable_. You should also create an initializer for this class that takes one parameter and assigns that parameter to the `fahrenheit` property.
  */
 // write your code here
+class Thermometer{
+    var fahrenheit:Double
+    var celsius:Double{
+        get{
+            return ((fahrenheit - 32)*(5/9))
+        }
+        
+        set {
+            fahrenheit = newValue * (9/5) + 32
+            
+        }
+    }
+    
+    var description:String {
+        return("The current temperature is \(fahrenheit)°F (\(celsius)°C)")
+    }
+    
+
+    init(fahrenheit:Double){
+        self.fahrenheit = fahrenheit
+    }
+}
+
+
 
 /*: section2
  
@@ -22,6 +46,7 @@
  */
 // write your code here
 
+var thermometer = Thermometer(fahrenheit: 88.0)
 assert(thermometer.fahrenheit == 88.0, "thermometer.fahrenheit is \(thermometer.fahrenheit)")
 
 /*: section3
@@ -42,6 +67,7 @@ assert(thermometer.celsius - 31.1 < 0.1, "thermometer.celsius is \(thermometer.c
  */
 // write your code here
 
+thermometer.fahrenheit = 100.0
 assert(thermometer.fahrenheit == 100.0, "thermometer.fahrenheit is \(thermometer.fahrenheit)")
 assert(thermometer.celsius - 37.7 < 0.1, "thermometer.celsius is \(thermometer.celsius)")
 
@@ -58,6 +84,8 @@ assert(thermometer.celsius - 37.7 < 0.1, "thermometer.celsius is \(thermometer.c
  Now set the `celsius` temperature of your `Thermometer` instance to 100.0
  */
 // write your code here
+
+thermometer.celsius = 100.0
 
 assert(thermometer.celsius == 100.0, "thermometer.celsius is \(thermometer.celsius)")
 assert(thermometer.fahrenheit == 212.0, "thermometer.fahrenheit is \(thermometer.fahrenheit)")
